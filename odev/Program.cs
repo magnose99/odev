04d58e9e81ca6a1780fcfb96c2f4bd1eb6aaa11f
Furkancan <97769468+magnose99@users.noreply.github.com>
2022-01-30 03:48:54 +0300

    Add files via upload

---
+++ odev/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+    Console.WriteLine("Hello World");
+    Console.WriteLine("İsminizi Girin");
+    string name = Console.ReadLine();
+    Console.WriteLine("Soyisminizi Girin");
+    string surname=Console.ReadLine();
+    Console.WriteLine("Merhaba "+ name + " " + surname);
+
+
+    }
+
+}
+
